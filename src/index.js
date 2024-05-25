9ec000fef6e2750d45dd29ade67fd2aff7417913
MxsonnGithub <166234561+MxsonnGithub@users.noreply.github.com>
2024-05-25 15:15:59 -0500

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -8,19 +8,19 @@ import {
 } from 'react-router-dom'
 
 import './style.css'
-import Error from './views/error'
 import Home from './views/home'
 import Links from './views/links'
 import LockdownError from './views/lockdown-error'
+import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Error} path="**" />
         <Route component={Home} exact path="/" />
         <Route component={Links} exact path="/links" />
         <Route component={LockdownError} exact path="/lockdown" />
+        <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
     </Router>
